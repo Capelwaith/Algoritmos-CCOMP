2d10d65f658be0fc34f9e4b2ba1f2d6cddc9f406
Bryan Andre Ojeda Cruz <Bryan Andre Ojeda Cruz>
2016-09-23 09:49:03 -0500

    Archivo2

--- REPOSITORIO/main.c
+++ REPOSITORIO/main.c
@@ -1,131 +1,47 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
-int lower()
+#include <stdbool.h>
+int concatenar(char *palabra, char *palabra_2)
 {
-    char c, minuscula;
-    while(c<'Z' && c>'A');
-        printf("\nInserte la letra en mayuscula:\n");
-        scanf("%c", &c);
-        minuscula=c+' ';
-        printf("%c\n", minuscula);
-        printf("\n\n");
-
-}
-int strindex(char palabra[], int t, int s)
-{
-    /*Posicion desde punto de vista del programador*/
-    int posicion;
-    posicion=t+s;
-    if(palabra[t+s]=='\0' || posicion>strlen(palabra))
+    char cadena[15];
+    int pos=0;
+    int n=1;
+    while(n<=strlen(palabra)+strlen(palabra_2))
     {
-        printf("La posicion no se encuentra, se retornara -1\n");
-        return -1;
-    }
-    else
-        printf("El caracter es %c y la posicion es %d\n",palabra[t+s], posicion);
-
-
-}
-int conversor(char num[])
-{
-
-	int i,x, len, pot_1, pot_2;
-	float dec= 0.0, temp=0.0, temp_2;
-	len = strlen(num);
-	for(i=0; i<len; i++)
-    {
-        if(num[i]=='.')
+        n++;
+        cadena[pos]=*palabra;
+        palabra++;
+        pos++;
+        if(*palabra=='\0')
         {
-            pot_1=i+1;
-            for(x=i+1; x<len; x++)
-            {
-                temp= temp*10.0 + ( num[x] - '0' );
-            }
-            pot_2=len-pot_1;
-            temp_2=temp/pow(10,pot_2);
-            dec+=temp_2;
-            return dec;
-
+            palabra=&*palabra_2;
         }
 
-
-        else
-            dec = dec * 10.0 + ( num[i] - '0' );
-
-	}
-
-}
-float elevado(char expo[])
-{
-    int i;
-    float ex=0;
-    if(expo[0]!='e')
-    {
-        printf("Exponente debe llevar una e");
-        return 0;
     }
-    else
+    printf("%s", cadena);
+    return 0;
+}
+strend(char *palabra, char *palabra_2)
+{
+    while(true)
     {
-        for (i=2;i<strlen(expo);++i)
+        palabra++;
+        if(*palabra==*palabra_2)
         {
-            ex= ex*10.0 + ( expo[i] - '0' );
+            if(*palabra=='\0' && *palabra_2=='\0')
+                return 1;
+            else
+                palabra_2++;
         }
-        ex= (pow(10, ex));
-        return ex;
+        else if(*palabra=='\0')
+            return 0;
     }
-
-
-}
-conversion(char num[], char expo[])
-{
-    float x, y, resultado;
-    int h;
-
-    if(expo[1]=='-')
-    {
-        x=conversor(num);
-        y=elevado(expo);
-        resultado=x/y;
-        printf("%f", resultado);
-        return resultado;
-    }
-    else
-    {
-        float x, y, resultado;
-        x=conversor(num);
-        y=elevado(expo);
-        resultado=x*y;
-        printf("%f \n", resultado);
-        return resultado;
-    }
-
-
-
 }
 
-void invertir(char palabra[])
+int main()
 {
-    /*No pude hacerlo recursivo*/
-    int lenght;
-    int x, ult_pos;
-    printf("%s\n", palabra);
-    for(lenght=0; palabra[lenght]!='\0'; lenght++);
-    ult_pos=lenght-1;
-    for (x=ult_pos; x>=0; x--)
-        printf("%c", palabra[x]);
-
-
-
-
-
-}
-main(void)
-{
-    conversion("123.45", "e-6");
-    lower();
-    strindex("Hola", 2, 1);
-    strindex("Hola", 0, 2);
-    strindex("Hola", 2, 64);
-    invertir("ENCRIPTEMOS ARCHIVOS");
+    concatenar("ola"," k ase");
+    strend("hola", "la");
+    strend("Hola", "NO");
 }
